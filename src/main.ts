b2fa9a8cb116e59b03d361a71c251dda9fa0264d
DeepakG <deepak@devbeeit.com>
2022-06-13 15:48:57 +0530

    make it better

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
-  const port = "https://todo-app-backend-server.herokuapp.com/" || 4000
+  const port = 4000 || process.env.PORT
   await app.listen(port);
 }
 bootstrap();
