782b4cb8640dbad3907244ba2eeefe035e88d9e1
DeepakG <deepak@devbeeit.com>
2022-06-13 16:04:13 +0530

    MAKE IT BETTER

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
-  const port = 4000 || process.env.PORT
-  await app.listen(port);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
